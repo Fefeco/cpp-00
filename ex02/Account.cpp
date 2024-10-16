31ffdf401a31d0ce6250b09f41504fcd9db4d5ca
Fefeco <carranzafederico@gmail.com>
2024-10-16 22:40:02 +0200

    Implement constructor for Account class

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:36:11 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/10/07 15:34:39 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/10/16 22:33:35 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,29 @@
 #include <iostream>
 #include <iomanip>
 #include <ctime>
+// #include <utility>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+
+Account::Account( int initial_deposit ) 
+    : _amount(initial_deposit),
+    _nbDeposits(0),
+    _nbWithdrawals(0)
+{
+    this->_accountIndex = t::_nbAccounts;
+    ++t::_nbAccounts;
+    t::_totalAmount += this->_amount;
+    t::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";"
+              << "amount:" << this->_amount << ";"
+              << "created" << std::endl;
+}
+
+Account::~Account( void ){}
 
 void Account::_displayTimestamp( void ) {
     std::time_t current_time = std::time( NULL );
@@ -27,7 +50,6 @@ void Account::_displayTimestamp( void ) {
               << std::setw(2) << time->tm_hour    
               << std::setw(2) << time->tm_min    
               << std::setw(2) << time->tm_sec
-              << "]"
-              << std::endl;
+              << "] ";
 }
-// [19920104_091532]
+// [19920104_091532] index:0;amount:42;created
