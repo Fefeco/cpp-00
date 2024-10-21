a1f2b62c0dcec9007b5b52a8fc5640a6925f00f1
Fefeco <carranzafederico@gmail.com>
2024-10-21 11:03:37 +0200

    Remove space sesparator from the function. The subject doensn't ask for them

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 10:23:18 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/10/03 18:14:56 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/10/21 11:01:35 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,6 @@ int	main(int argc, char **argv)
 			curr = 0;
 			while (argv[i][curr])
 				std::cout << (char)std::toupper(argv[i][curr++]);
-			if (i + 1 < argc)
-				std::cout << " ";
 		}
 	}
 	std::cout << std::endl;
